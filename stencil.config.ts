b2a66f9e75f123ab9fbbf3b3ce4a3bdca1d1d1ea
Newton Munene <newtonmunene99@gmail.com>
2018-08-26 19:24:05 +0300

    Init to win it.

---
+++ stencil.config.ts
@@ -0,0 +1,14 @@
+import { Config } from '@stencil/core';
+import { sass } from '@stencil/sass';
+export const config: Config = {
+  namespace: 'mycomponent',
+  outputTargets:[
+    {
+      type: 'dist'
+    },
+    {
+      type: 'www',
+      serviceWorker: null
+    }
+  ],plugins: [sass()]
+};
